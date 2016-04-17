aarentoft/aMap2016

commit cd2436f8d90efb19412a956a32fa2f7454613bfc

Improve RoadEdge toString() to not be half finished

src/util/graph/RoadEdge.java
@@ -57,7 +57,7 @@ public class RoadEdge extends Rectangle implements Comparable<RoadEdge> {
 
 	@Override
 	public String toString() {
-		String s = data.roadname + ", ";
+		String s = "[RoadEdge name=" + data.roadname + ", length=" + length + "]";
 		return s;
 	}
 

commit d6766fcdf0802d472cae166a0f20845d80ab6aa4

Fix regression where road name search is broken
This regression was introduced in commit
cd2436f8d90efb19412a956a32fa2f7454613bfc and is a result of the fact
that Trie ordering was dependant on the toString() method of the ordered
object type. The user would have to search the toString() representation
of the road, so when that representation changed from just <name-of-road> to
[RoadEdge roadname="<name-of-road>"..., the user would have to write
[RoadEdge roadname="<name-of-road>" everytime a search was performed for
the Trie to find anything.

With this commit, Trie ordering now uses a separate string representation
as this allows for a more informative toString() representation while
allowing the user to search simply by the name of the road.

src/util/Trie.java
@@ -12,7 +12,7 @@ import java.util.TreeMap;
  * 
  * @param <T>
  */
-public class Trie<T> {
+public class Trie<T extends TrieSearchable> {
 	protected T element;
 	protected int level;
 	protected Map<Character, Trie<T>> children;
@@ -31,7 +31,7 @@ public class Trie<T> {
 	 * @param e
 	 */
 	public void insert(T e) {
-		insert(e, e.toString().toLowerCase());
+		insert(e, e.getTrieRepresentation().toLowerCase());
 	}
 
 	/**

src/util/TrieSearchable.java
@@ -1,0 +1,14 @@
+package util;
+
+/**
+ * Classes which is going to be a part of the {@link Trie} structure must implement this interface.
+ */
+public interface TrieSearchable {
+    /**
+     * Returns a string representation used by the {@link Trie} structure to put the implementing object
+     * in the correct order in the trie.
+     *
+     * @return the trie ordering string
+     */
+    public String getTrieRepresentation();
+}

src/util/graph/RoadEdge.java
@@ -3,11 +3,12 @@ package util.graph;
 import java.util.HashMap;
 
 import util.Rectangle;
+import util.TrieSearchable;
 
 /**
  * A graph edge created from an EdgeData object.
  */
-public class RoadEdge extends Rectangle implements Comparable<RoadEdge> {
+public class RoadEdge extends Rectangle implements Comparable<RoadEdge>, TrieSearchable {
 	// Length in meters
 	public final double length;
 	public final RoadNode start;
@@ -53,6 +54,11 @@ public class RoadEdge extends Rectangle implements Comparable<RoadEdge> {
 	 */
 	public RoadNode getOther(RoadNode node) {
 		return (node.equals(start)) ? end : start;
+	}
+
+	@Override
+	public String getTrieRepresentation() {
+		return data.roadname;
 	}
 
 	@Override

src/view/LiveComboBox.java
@@ -67,7 +67,7 @@ public class LiveComboBox extends JComboBox implements Observer {
 			addItem(enteredString);
 			if (results != null) {
 				for (RoadEdge edge : results) {
-					addItem(edge);
+					addItem(edge.data.roadname);
 				}
 			}
 			setPopupVisible(true);